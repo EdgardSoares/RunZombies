c2fb2843378d18448b6979d37838a8d296608ba9
Edgard Thobias <edgardsoares_ed@hotmail.com>
2020-05-23 04:53:07 -0400

    - Foi adicionado uma nova Cena chamada: Menu - Foi adicionado mais cameras virtuais - Foi adicionado Timeline - Foi adicionado scripts para o menu de botao de Start do jogo - Foi adicionado UI de Menu Principal e Tutorial

---
+++ RunZombiesProject/Assets/Scripts/Menu/StartButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour
+{
+
+    public GameObject _startText;
+    public GameObject _tutorialText;
+    public void OnMouseDown()
+    {
+        _startText.SetActive(false);
+        _tutorialText.SetActive(true);
+    }
+}
---
+++ RunZombiesProject/Assets/Scripts/Menu/pressSpace.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pressSpace : MonoBehaviour
+{
+    
+    void Update()
+    {
+        NextScene();
+    }
+
+    void NextScene()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+    }
+}
